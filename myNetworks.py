0904c8afa73067611778e74e680ab43b1f9abe9f
Delbo01 <leonardo.delbene@stud.unifi.it>
2025-04-09 15:32:39 +0200

    first commit

---
+++ myNetworks.py
@@ -0,0 +1,239 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class MLP(nn.Module):
+    def __init__(self, layer_sizes):
+        super().__init__()
+        self.num_layers = len(layer_sizes) - 1
+        self.layers = nn.ModuleList([
+            nn.Linear(layer_sizes[i], layer_sizes[i+1]) for i in range(self.num_layers)
+        ])
+
+    def forward(self, x):
+        x = x.view(x.shape[0], -1)
+        for i in range(self.num_layers - 1):
+            x = F.relu(self.layers[i](x))
+        return self.layers[-1](x)
+
+
+class MlP_Block(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(MlP_Block, self).__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, input_size)
+
+    def forward(self, x):
+        residual = x
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        return x + residual
+
+class Residual_MLP(nn.Module):
+    def __init__(self, num_blocks, input_size, hidden_size):
+        super(Residual_MLP, self).__init__()
+        self.blocks = nn.ModuleList([MlP_Block(hidden_size, hidden_size) for _ in range(num_blocks)])
+        self.final_linear = nn.Linear(hidden_size, 10)
+        self.initial_linear = nn.Linear(input_size, hidden_size)
+
+    def forward(self, x):
+        x = x.view(x.shape[0], -1)
+        x = F.relu(self.initial_linear(x))
+        for block in self.blocks:
+            x = block(x)
+        x = F.relu(self.final_linear(x))
+        return x
+
+class CNN_Block(nn.Module):
+    def __init__(self, in_channels, out_channels, num_layers):
+        super(CNN_Block, self).__init__()
+        layers = []
+        for _ in range(num_layers):
+            layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1))
+            layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.ReLU(inplace=True))  # ReLU viene eseguito dopo ogni convoluzione
+
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.block(x)
+        return x
+
+class Transition_Layer(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 2):
+        super(Transition_Layer, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.pool = nn.AvgPool2d(kernel_size=2, stride=stride)
+
+    def forward(self, x):
+        x = F.relu(self.bn(self.conv(x)))
+        return self.pool(x)
+
+class CNN(nn.Module):
+    def __init__(self, num_blocks, input_channels, hidden_channels, num_class, dim_input):
+        super(CNN, self).__init__()
+        self.num_blocks = num_blocks
+        blocks = []
+        for i in range(len(num_blocks)):
+            blocks.append(CNN_Block(hidden_channels[i], hidden_channels[i], num_blocks[i]))
+        self.layers = nn.ModuleList(blocks)
+        trans = []
+        for i in range(1, len(num_blocks)):
+            trans.append(Transition_Layer(hidden_channels[i - 1], hidden_channels[i]))
+        self.trans = nn.ModuleList(trans)
+
+        dim = dim_input
+        for _ in range(len(trans)):
+            dim = dim // 2
+
+        self.initial_conv = nn.Conv2d(input_channels, hidden_channels[0], kernel_size=3, padding=1)
+
+        self.fc = nn.Linear(hidden_channels[-1] * (dim) * (dim), num_class)
+    def forward(self, x):
+        x = F.relu(self.initial_conv(x))
+        for i in range(len(self.layers)):
+            x = self.layers[i](x)
+            if (i < len(self.trans)):
+                x = self.trans[i](x)
+
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
+
+
+class Residual_Block(nn.Module):
+    def __init__(self, channels):
+        super(Residual_Block, self).__init__()
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(channels)
+
+    def forward(self, x):
+        residual = x
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        return x + residual
+
+class Block(nn.Module):
+    def __init__(self, channels, num_layers):
+        super(Block, self).__init__()
+        layers = []
+        for _ in range(num_layers):
+            layers.append(Residual_Block(channels))
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.block(x)
+
+class Transition(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Transition, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x= F.relu(self.bn(self.conv(x)))
+        return self.pool(x)
+
+class ResNet(nn.Module):
+    def __init__(self, channels, num_blocks, input_channels, num_class):
+        super(ResNet, self).__init__()
+        blocks = []
+        for i in range(len(num_blocks)):
+            blocks.append(Block(channels[i], num_blocks[i]))
+        self.layers = nn.ModuleList(blocks)
+        self.initial_conv = nn.Conv2d(input_channels, channels[0], kernel_size=3, padding=1)
+        trans = []
+        for i in range(1,len(num_blocks)):
+          trans.append(Transition(channels[i-1], channels[i]))
+        self.trans = nn.ModuleList(trans)
+        dim = 32
+        for _ in range(len(trans)):
+          dim = dim // 2
+        self.fc = nn.Linear(channels[-1] * dim * dim, num_class)
+
+    def forward(self, x):
+        x = F.relu(self.initial_conv(x))
+        for i in range(len(self.layers)):
+          x = self.layers[i](x)
+          if (i < len(self.trans)):
+            x = self.trans[i](x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
+
+
+class CNN_CAM(nn.Module):
+    def __init__(self, num_blocks, input_channels, hidden_channels, num_class):
+        super(CNN_CAM, self).__init__()
+        self.num_blocks = num_blocks
+        blocks = []
+        for i in range(len(num_blocks)):
+            blocks.append(CNN_Block(hidden_channels[i], hidden_channels[i], num_blocks[i]))
+        self.layers = nn.ModuleList(blocks)
+
+        trans = []
+        for i in range(1, len(num_blocks)):
+            trans.append(Transition_Layer(hidden_channels[i - 1], hidden_channels[i], stride=1))
+        self.trans = nn.ModuleList(trans)
+
+        self.initial_conv = nn.Conv2d(input_channels, hidden_channels[0], kernel_size=3, padding=1)
+
+        self.fc = nn.Linear(hidden_channels[-1], num_class)
+        self.gap = nn.AdaptiveAvgPool2d(1)
+
+    def forward_features(self, x):
+        x = F.relu(self.initial_conv(x))
+        for i in range(len(self.layers)):
+            x = self.layers[i](x)
+            if i < len(self.trans):
+                x = self.trans[i](x)
+        return x
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.gap(x)  # Pooling medio adattivo
+        x = torch.flatten(x, 1)  # Flatten per il layer fully connected
+        x = self.fc(x)  # Passaggio attraverso il fully connected layer
+        return x
+
+class CAM():
+    def __init__(self, model, type='resnet'):
+        self.model = model.eval()
+        self.type = type
+        self.feature = nn.Sequential(*list(self.model.children())[:-2])
+        self.fc_weights = self.model.fc.weight.detach()  # Pesi del layer finale (fc)
+
+    def generate(self, input):
+        with torch.no_grad():
+            # Estrai le attivazioni della parte convoluzionale
+            if(self.type == "resnet"):
+                features = self.feature(input)
+            else:
+                features = self.model.forward_features(input)
+        # Calcola le probabilità di output
+        output = self.model(input)
+
+        # Ottieni l'indice della classe prevista
+        class_idx = torch.argmax(output).item()
+
+        # Ottieni i pesi associati alla classe prevista
+        class_weights = self.fc_weights[class_idx]
+
+        # Ridimensiona le attivazioni (features) per il calcolo della CAM
+        features = features.squeeze(0)  # Rimuovi la dimensione del batch
+
+        # Calcola la mappa di attivazione (CAM)
+        # Le attivazioni sono di forma [C, H, W] dove C è il numero di canali (es. 512)
+        cam = torch.sum(class_weights[:, None, None] * features, dim=0)  # Somma lungo i canali
+
+        # Normalizza la CAM
+        cam = cam - cam.min()
+        cam = cam / (cam.max() + 1e-8)
+
+        return cam.cpu().numpy()
---
+++ pipeline.py
@@ -0,0 +1,152 @@
+import wandb
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+
+def train_epoch(model, train_loss, correct_train, total_train, train_loader, opt, criterion, device='cpu',):
+
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+        #images = images.view(images.shape[0], -1)
+
+        opt.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        opt.step()
+
+        train_loss += loss.item()
+        _, predicted = torch.max(outputs, 1)
+        correct_train += (predicted == labels).sum().item()
+        total_train += labels.size(0)
+
+    return train_loss, correct_train, total_train
+
+
+def valid_epoch(model, val_loss, correct_val, total_val, val_loader, criterion, device='cpu'):
+
+  with torch.no_grad():
+    for images, labels in val_loader:
+      images, labels = images.to(device), labels.to(device)
+      #images = images.view(images.shape[0], -1)
+
+      outputs = model(images)
+      loss = criterion(outputs, labels)
+      val_loss += loss.item()
+
+      _, predicted = torch.max(outputs, 1)
+      correct_val += (predicted == labels).sum().item()
+      total_val += labels.size(0)
+
+  return val_loss, correct_val, total_val
+
+def test(model, test_loss, correct_test, total_test, test_loader, criterion, device='cpu'):
+
+  with torch.no_grad():
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+        #images = images.view(images.shape[0], -1)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        test_loss += loss.item()
+
+        _, predicted = torch.max(outputs, 1)
+        correct_test += (predicted == labels).sum().item()
+        total_test += labels.size(0)
+
+  return test_loss, correct_test, total_test
+
+
+def train_validate(model, num_epochs, train_loader, val_loader, opt, criterion, device, filename, checkpoint_filename=None):
+    best_val_acc = 0.0
+    best_val_loss = float('inf')
+    start_epoch = 0
+
+    # Carica il checkpoint se esiste
+    if checkpoint_filename and os.path.exists(checkpoint_filename):
+        start_epoch, best_val_acc, best_val_loss = load_checkpoint(model, opt, checkpoint_filename)
+
+    for epoch in range(start_epoch, num_epochs):
+        model.train()
+        train_loss, correct_train, total_train = 0.0, 0, 0
+        train_loss, correct_train, total_train = train_epoch(model, train_loss, correct_train, total_train,
+                                                             train_loader, opt, criterion, device)
+
+        train_loss /= len(train_loader)
+        train_acc = correct_train / total_train * 100
+
+        # Validazione
+        model.eval()
+        val_loss, correct_val, total_val = 0.0, 0, 0
+        val_loss, correct_val, total_val = valid_epoch(model, val_loss, correct_val, total_val, val_loader, criterion,
+                                                       device)
+
+        val_loss /= len(val_loader)
+        val_acc = correct_val / total_val * 100
+
+        # Logga i valori su WandB
+        wandb.log({
+            "Train Loss": train_loss,
+            "Train Accuracy": train_acc,
+            "Validation Loss": val_loss,
+            "Validation Accuracy": val_acc
+        })
+
+        print(f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.2f}%, "
+              f"Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.2f}%")
+
+        if val_acc > best_val_acc and val_loss < best_val_loss:
+            best_val_acc = max(val_acc, best_val_acc)
+            best_val_loss = min(val_loss, best_val_loss)
+            torch.save(model.state_dict(), filename)
+            print(f"🧠 Modello migliorato, salvato in: {filename}")
+            wandb.save(filename)
+            if(checkpoint_filename):
+                # Salva il checkpoint
+                save_checkpoint(model, opt, epoch, best_val_acc, best_val_loss, checkpoint_filename)
+                artifact = wandb.Artifact('model_checkpoint', type='model')
+                artifact.add_file(checkpoint_filename)
+                wandb.log_artifact(artifact)
+
+
+def evaluate_model(model, test_loader, criterion, device):
+    model.eval()
+    test_loss, correct_test, total_test = 0.0, 0, 0
+    test_loss, correct_test, total_test = test(model, test_loss, correct_test, total_test, test_loader, criterion, device)
+
+    test_loss /= len(test_loader)
+    test_acc = correct_test / total_test * 100
+
+    # Logga il test su WandB
+    wandb.log({
+        "Test Loss": test_loss,
+        "Test Accuracy": test_acc
+    })
+
+    print(f"🔹 Test Loss: {test_loss:.4f}, Test Accuracy: {test_acc:.2f}%")
+
+    wandb.finish()  # Chiude il logging di WandB
+
+
+def save_checkpoint(model, optimizer, epoch, best_val_acc, best_val_loss, filename):
+    checkpoint = {
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'best_val_acc': best_val_acc,
+        'best_val_loss': best_val_loss,
+    }
+    torch.save(checkpoint, filename)
+    print(f"Checkpoint salvato in {filename}")
+
+def load_checkpoint(model, optimizer, filename):
+    checkpoint = torch.load(filename)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    best_val_acc = checkpoint['best_val_acc']
+    best_val_loss = checkpoint['best_val_loss']
+    print(f"Checkpoint caricato da {filename}, riprendo dall'epoca {epoch}")
+    return epoch, best_val_acc, best_val_loss
